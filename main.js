d9d78e0470f9ad7dadd248d6a05b9643ea19708a
the-supernova <biswalabhijit04@gmail.com>
2022-05-20 08:41:26 +0530

    Create SDP answer

--- main.js
+++ main.js
@@ -25,6 +25,7 @@ let init = async () => {
     await channel.join();
 
     channel.on('MemberJoined', handleUserJoined)
+    channel.on('MemberLeft', handleUserLeft)
 
     client.on('MessageFromPeer', handleMessageFromPeer)
 
@@ -32,9 +33,25 @@ let init = async () => {
     document.getElementById('user-1').srcObject = localStream;
 }
 
+let handleUserLeft = (MemberId) => {
+    document.getElementById('user-2').style.display = 'none';
+}
+
 let handleMessageFromPeer = async (message, MemberId) => {
     message = JSON.parse(message.text)
-    console.log('Message:', message)
+    if(message.type === 'offer') {
+        createAnswer(MemberId, message.offer)
+    }
+
+    if(message.type === 'answer') {
+        addAnswer(message.answer)
+    }
+
+    if(message.type === 'candidate') {
+        if(peerConnection) {
+            peerConnection.addIceCandidate(message.candidate)
+        }
+    }
 }
 
 let handleUserJoined = async (MemberId) => {
@@ -42,11 +59,12 @@ let handleUserJoined = async (MemberId) => {
     createOffer(MemberId)
 }
 
-let createOffer = async (MemberId) => {
+let createPeerConnection = async (MemberId) => {
     peerConnection = new RTCPeerConnection(servers)
 
     remoteStream = new MediaStream()
     document.getElementById('user-2').srcObject = remoteStream;
+    document.getElementById('user-2').style.display = 'block';
 
     if(!localStream) {
         localStream = await navigator.mediaDevices.getUserMedia({ video: true, audio: false });
@@ -68,11 +86,39 @@ let createOffer = async (MemberId) => {
             client.sendMessageToPeer({text:JSON.stringify({'type':'candidate', 'candidate':event.candidate})}, MemberId)
         }
     }
+}
+
+let createOffer = async (MemberId) => {
+    await createPeerConnection(MemberId)
 
     let offer = await peerConnection.createOffer();
     await peerConnection.setLocalDescription(offer);
 
     client.sendMessageToPeer({text:JSON.stringify({'type':'offer', 'offer':offer})}, MemberId)
 }
+
+let createAnswer = async (MemberId, offer) => {
+    await createPeerConnection(MemberId)
+
+    await peerConnection.setRemoteDescription(offer)
+
+    let answer = await peerConnection.createAnswer()
+    await peerConnection.setLocalDescription(answer)
+
+    client.sendMessageToPeer({text:JSON.stringify({'type':'answer', 'answer':answer})}, MemberId)
+}
+
+let addAnswer = async (answer) => {
+    if(!peerConnection.currentRemoteDescription) {
+        peerConnection.setRemoteDescription(answer)
+    }
+}
+
+let leaveChannel = async () => {
+    await channel.leave()
+    await client.logout()
+}
+
+window.addEventListener('beforeunload', leaveChannel)
 
 init()
